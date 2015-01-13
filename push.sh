0d0d6a03d34cbeda00d36fad06e25211203e4f0c
Rogach <platon7pronko@gmail.com>
2015-01-13 09:21:23 +0300

    add push script

---
+++ push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbt assembly && rsync -avz --progress -e "ssh -o GSSAPIAuthentication=no -p 2222" target/scala-2.11/avalanche-1.8.0.jar platon@meg.mutokukai.ru:/data/programs/platon/bin/avalanche.jar
